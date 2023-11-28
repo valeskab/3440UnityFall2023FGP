116d18fe4f86c25e14cc197c179e57d13c012872
Valeska <valeskabagaric@hotmail.com>
2023-11-27 19:21:51 -0700

    Updated player interaction

--- Projects/SandiaFruitGameFall2023/Assets/Scripts/UpdatedCode/Player/PlayerControllerBehaviour.cs
+++ Projects/SandiaFruitGameFall2023/Assets/Scripts/UpdatedCode/Player/PlayerControllerBehaviour.cs
@@ -8,21 +8,79 @@ public class PlayerControllerBehaviour : MonoBehaviour
 
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private GameInput gameInput;
-    
-    private void Update()
+    [SerializeField] private LayerMask countersLayerMask;
+
+    private Vector3 lastInteractDir;
+
+    private void Start()
+    {
+        gameInput.OnInteractAction += GameInput_OnInteractAction;
+    }
+
+    private void GameInput_OnInteractAction(object sender, System.EventArgs e)
     {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
 
-        float playerRadius = .7f;
+        if (moveDir != Vector3.zero)
+        {
+            lastInteractDir = moveDir;
+        }
+
+        float interactDistance = 2f;
+
+        if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, countersLayerMask))
+        {
+            if (raycastHit.transform.TryGetComponent(out ClearCounterBehaviour clearCounter))
+            {
+                clearCounter.Interact();
+            }
+        }
+    }
+
+    private void Update()
+    {
+        HandleMovement();
+        HandleInteractions();
+    }
+
+    private void HandleInteractions()
+    {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+
+        if (moveDir != Vector3.zero)
+        {
+            lastInteractDir = moveDir;
+        }
+
+        float interactDistance = 2f;
+
+        if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, countersLayerMask))
+        {
+            if (raycastHit.transform.TryGetComponent(out ClearCounterBehaviour clearCounter))
+            {
+                
+            }
+        }
+    }
+
+    private void HandleMovement()
+    {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+
+        float playerRadius = 1f;
         float playerHeight = 2f;
         float moveDistance = moveSpeed * Time.deltaTime;
         bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
         
         if (!canMove)
         {
-            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0);
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
             
             canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
 
@@ -32,7 +90,7 @@ public class PlayerControllerBehaviour : MonoBehaviour
             }
             else
             {
-                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z);
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
             
                 canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
                 if (canMove)
